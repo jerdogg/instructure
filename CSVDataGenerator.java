88a6fd6b803c35aa88204c90e66caa86a5afd6d0
Jeremy Bird <Jeremykbird@gmail.com>
2016-02-20 14:39:07 -0700

    Create CSV data generator for use in Junit test cases

---
+++ CSVDataGenerator.java
@@ -0,0 +1,84 @@
+package instructure;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.opencsv.CSVWriter;
+
+public class CSVDataGenerator {
+
+	public static Set<Student> buildValidStudents() throws IOException {
+
+		Set<Student> studentSet = new HashSet<Student>();
+
+		for (int i = 0; i < 5; i++) {
+			Student student = new Student(i);
+			student.set_name("Student Name " + i);
+			student.set_courseID(i * 3);
+			student.set_active(i % 2 == 0);
+			studentSet.add( student );
+		}
+
+		writeCSV("./bin/instructure/inputfiles/students.csv",  toStringArrayStudents(studentSet, true) );	
+		return studentSet;
+	}
+	
+
+	public static Set<Course> buildValidCourses() throws IOException{
+		
+		Set<Course> courseSet = new HashSet<Course>();
+
+		for (int i = 0; i < 5; i++) {
+			Course course = new Course(i);
+			course.set_courseName("Course Name "+i);
+			course.set_state(i%2==0);
+			courseSet.add(course);
+		}
+
+		writeCSV("./bin/instructure/inputfiles/classes.csv", toStringArrayCourse(courseSet, true) );	
+		return courseSet;
+	}
+	
+	
+	
+	
+	private static void writeCSV(String fileName, List<String[]> data) throws IOException{
+		CSVWriter csvWriter = new CSVWriter(new FileWriter(fileName), ',');
+		csvWriter.writeAll(data);
+		csvWriter.close();	
+	}
+
+	private static List<String[]> toStringArrayStudents(Set<Student> studentSet, boolean withHeader) {
+
+		List<String[]> records = new ArrayList<String[]>();
+		
+		if (withHeader)
+			records.add(new String[] { "user_id", "user_name", "couse_id", "state" });
+		
+		for (Student stud : studentSet ) {
+			records.add(new String[] { String.valueOf(stud.get_studentId()), stud.get_name(),
+					String.valueOf(stud.get_courseID()), String.valueOf(stud.is_active()) });
+		}
+		return records;
+	}
+	
+	private static List<String[]> toStringArrayCourse(Set<Course> courseSet, boolean withHeader) {
+
+		List<String[]> records = new ArrayList<String[]>();
+		
+		if (withHeader)
+			records.add(new String[] { "course_id", "course_name", "state" });
+		
+		for (Course clas : courseSet ) {
+			records.add(new String[] { 	String.valueOf(clas.get_courseID() ), 
+										clas.get_courseName(),
+										String.valueOf(clas.is_state()) });
+		}
+		return records;
+	}
+	
+}
